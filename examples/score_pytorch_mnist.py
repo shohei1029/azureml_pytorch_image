2039af129ed9afa9be8df2e81b1c972180413811
Shohei N <shohei.nagata@microsoft.com>
2021-07-09 01:57:58 +0900

    minor changes

---
+++ examples/score_pytorch_mnist.py
@@ -0,0 +1,58 @@
+import os
+import json
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+def init():
+    global model
+    # AZUREML_MODEL_DIR is an environment variable created during deployment.
+    # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
+    # For multiple models, it points to the folder containing all deployed models (./azureml-models)
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'mnist_cnn.pt')
+    model = Net()
+    model.load_state_dict(
+        torch.load(model_path, map_location=torch.device('cpu')))
+    model.eval()
+
+
+def run(input_data):
+    input_data = torch.tensor(json.loads(input_data)['data'])
+
+    # get prediction
+    with torch.no_grad():
+        input_data = input_data.unsqueeze(0)  # add batch dimension
+        output = model(input_data)
+        softmax = nn.Softmax(dim=1)
+        pred_probs = softmax(output).numpy()[0]
+        index = torch.argmax(output, 1)
+
+    result = {"label": int(index), "probability": str(pred_probs[index])}
+    return result
